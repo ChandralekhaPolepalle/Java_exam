e8586ef8073d0ea70aefcb872427a4cee426655e
Chandralekha <pchandralekha99@gmail.com>
2023-12-09 11:28:36 -0500

    feat: Created and implemented modulo

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,12 @@ public class Main {
                 new Number(3),
                 new Product(new Number(2),new Number(5))
         );
-        System.out.println("Result: " + exp.evaluate());
+        ArithmeticExp modulo_exp = new Sum(
+                new Number(3),
+                new Product(new Number(2),new Modulo(new Number(11), new Number(2)))
+        );
+        System.out.println("Result of "+ exp.toString()+" : " + exp.evaluate());
+        System.out.println("Result of "+ modulo_exp.toString()+" : " + modulo_exp.evaluate());
 
     }
 }
---
+++ src/Modulo.java
@@ -0,0 +1,11 @@
+public class Modulo extends Binary {
+
+    public Modulo(ArithmeticExp left, ArithmeticExp right) {
+        super(left, right);
+    }
+
+    @Override
+    public int evaluate() {
+        return left.evaluate() % right.evaluate();
+    }
+}
